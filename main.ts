79d79b63a935d5e0415631c49250d99e973ec887
Kaleid0sc0pe <k.linder@mcaabhm.org>
2024-09-30 13:16:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,6 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    number_string3 = number1 + number2
-    game.splash(number_string3)
-    number1 = number2
-    number2 = number_string3
-})
 let mySprite: Sprite = null
-let number_string3 = 0
-let number2 = 0
-let number1 = 0
-number1 = 1
-number2 = 1
+let number1 = 1
+let number2 = 1
 game.onUpdateInterval(500, function () {
     mySprite = sprites.create(img`
         . . . . . . 2 2 2 2 . . . . . . 
